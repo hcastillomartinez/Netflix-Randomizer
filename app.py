0da68e94cab604fc78c62d143d8ae3a99bb7a792
Hector Castillo Martinez <hcastillomartinez@unm.edu>
2020-07-15 17:51:28 -0500

    creating base flask project and adding bootstrap functionality

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
