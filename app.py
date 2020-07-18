d9d33a3d4d91b945b0c3108369396a040fe441f9
Hector Castillo Martinez <hcastillomartinez@unm.edu>
2020-07-17 22:42:31 -0500

    update about page with typo, change login and finished my pool page. DB is complete and operational

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template, url_for, redirect, request, session
+from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import csv
 import random
 import requests
+import json
 import config
 
 df = pd.read_csv('netflix_titles.csv', header='infer')
 app = Flask(__name__)
 app.secret_key = config.SECRET_KEY
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    title_pool = db.Column(db.String())
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+
+
 
 @app.route('/')
 def index():
@@ -18,14 +33,33 @@ def index():
     return redirect(url_for('login'))
 
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-      session['username'] = request.form['username']
-      return redirect(url_for('index'))
+      username = request.form['username']
+      # check if username is already taken
+      query = Todo.query.filter_by(username=username)
+      print(query.count())
+      if query.count() == 0:
+        # adding new user to db
+        session['username'] = username
+        new_entry = Todo(username=username, title_pool=json.dumps([]))
+        try:
+            db.session.add(new_entry)
+            db.session.commit()
+            print('success adding user')
+            return redirect(url_for('index'))
+        except:
+            return "Problem creating user"
+
+      else:
+          # returning or collision in usernames
+          session['username'] = username
+          return redirect(url_for('index'))
     
+    # GET request
     return render_template('login.html')
+
 
 @app.route('/logout')
 def logout():
@@ -44,16 +78,55 @@ def about():
 @app.route('/my_pool', methods=['GET', 'POST'])
 def my_pool():
     if request.method == 'POST':
-        # movies_selected = request.form.getlist('movies')
-        # movies_selected = request.form['movies']
-        # print(str(movies_selected))
-        print(request.form['title'])
-        return redirect('/about')
+        title = request.form['title']
+        user = Todo.query.filter_by(username=session['username']).first()
+        try:
+            pool_as_list = json.loads(user.title_pool)
+            pool_as_list.append(title)
+            user.title_pool = json.dumps(pool_as_list)
+            db.session.commit()
+            return redirect(url_for('my_pool', pool=pool_as_list))
+        except:
+            return "Error: updating user pool"
     
     if 'username' in session:
-        return render_template('my_pool.html')
+        #get user from db and serve the list
+        user = Todo.query.filter_by(username=session['username']).first()
+        try:
+            pool = json.loads(user.title_pool)
+            return render_template('my_pool.html', pool=pool)
+        except:
+            return "Error: getting user pool %s" % user.title_pool
     
     return redirect(url_for('login'))
+
+# User pool functions
+
+@app.route('/delete/title=<id>')
+def delete(id):
+    user = Todo.query.filter_by(username=session['username']).first()
+    try:
+        pool_as_list = json.loads(user.title_pool)
+        pool_as_list.remove(id)
+        user.title_pool = json.dumps(pool_as_list)
+        db.session.commit()
+        return redirect(url_for('my_pool', pool=pool_as_list))
+    except:
+        return "Error: deleting title"
+
+
+# @app.route('/update')
+# def update():
+
+@app.route('/my_pool/random')
+def select_random_from_pool():
+    user = Todo.query.filter_by(username=session['username']).first()
+    try:
+        pool = json.loads(user.title_pool)
+        title = random.choice(pool)
+        return render_template('my_pool.html', title=title, pool=pool)
+    except:
+        return "Error: getting user pool %s" % user.title_pool
 
 @app.route('/random')
 def select_random():
